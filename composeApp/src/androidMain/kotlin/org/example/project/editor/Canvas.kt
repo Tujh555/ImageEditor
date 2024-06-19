b3fbbdb4b9b5350c0b8efdb644f6417dcf37d008
Egor A. Rusinov <erusinov@inno.tech>
2024-06-19 18:04:55 +0500

    cleanup

--- composeApp/src/androidMain/kotlin/org/example/project/editor/Canvas.kt
+++ composeApp/src/androidMain/kotlin/org/example/project/editor/Canvas.kt
@@ -1,11 +1,3 @@
 package org.example.project.editor
 
-import androidx.compose.ui.graphics.Canvas
-
-typealias ComposeCanvas = Canvas
 typealias AndroidCanvas = android.graphics.Canvas
-
-fun AndroidCanvas.asCompose() = Canvas(this)
-
-@Suppress("FunctionName")
-fun ComposeCanvas(other: AndroidCanvas) = Canvas(other)
